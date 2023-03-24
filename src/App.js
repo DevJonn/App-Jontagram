43ebef7a81f9326ed08eade3b6a0978ace2920fc
Jonathan Torres <jonathanlt68@gmail.com>
2023-03-24 11:10:34 -0300

    Updatev2
    Acrescentando input type

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import "firebase/compat/auth";
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 
 function App() {
+
+  const [user, setUser] = useState(null);
 
   useEffect(()=>{
     
   },[])
 
+
   return (
     <div className="App">
+      <div className='header'>
+
+
+      <div className='header__logo'>
+        <a href=''><img src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png' /> </a>
+      </div>
+
+      <div className='header__loginForm'>
+        <form>
+          <input type='text' placeholder='Login...' />
+          <input type='password' placeholder='Senha...' />
+          <input type='submit' nama='acao' value='Logar!' />
+        </form>
+      </div>
+
+
+      </div>
 
     </div>
   );
