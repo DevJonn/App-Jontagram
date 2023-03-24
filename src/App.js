97bd1617f26a06c08a820d52d94bc8314c41e38d
Jonathan Torres <jonathanlt68@gmail.com>
2023-03-24 14:23:49 -0300

    updatev3
    Estilizando o App

--- src/App.js
+++ src/App.js
@@ -14,22 +14,20 @@ function App() {
 
   return (
     <div className="App">
-      <div className='header'>
-
-
-      <div className='header__logo'>
+        <div className='header'>
+      <div className='center'>
+        <div className='header__logo'>
         <a href=''><img src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png' /> </a>
       </div>
 
-      <div className='header__loginForm'>
+        <div className='header__loginForm'>
         <form>
           <input type='text' placeholder='Login...' />
           <input type='password' placeholder='Senha...' />
           <input type='submit' nama='acao' value='Logar!' />
         </form>
       </div>
-
-
+      </div>
       </div>
 
     </div>
