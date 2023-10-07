1fafb89abc862d790993b2b3e0ad15e5d82dc008
Jonathan Torres <jonathanlt68@gmail.com>
2023-10-07 11:35:44 -0300

    Update
    Criando modal para criar conta

--- src/Header.js
+++ src/Header.js
@@ -7,29 +7,33 @@ function Header(props){
         
     }, [])
     
+    function abrirModalCriarConta(e){
+        e.preventDefault();
+    }
+
     return (
         
 
-        <div className='header'>
-<div className='center'>
-    <div className='header__logo'>
-        <a href=''><img src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png' /> </a>
-    </div>
-    {
+    <div className='header'>
+        <div className='center'>
+            <div className='header__logo'>
+                <a href=''><img src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png' /> </a>
+            </div>
+        {
         (props.user)?
         <div className='header__logadoInfo'>
-        <span>Olá, <b>{props.user} </b> </span>
-        <a href='#'>Postar!</a> 
+            <span>Olá, <b>{props.user} </b> </span>
+            <a href='#'>Postar!</a> 
         </div>
         :
-    <div className='header__loginForm'>
-        <form>
-        <input type='text' placeholder='Login...' />
-        <input type='password' placeholder='Senha...' />
-        <input type='submit' nama='acao' value='Logar!' />
-    </form>
-    <div className='btn__criarConta'>
-        <a href='#'>Criar Conta!</a>
+        <div className='header__loginForm'>
+            <form>
+                <input type='text' placeholder='Login...' />
+                <input type='password' placeholder='Senha...' />
+                <input type='submit' nama='acao' value='Logar!' />
+            </form>
+        <div className='btn__criarConta'>
+        <a onClick={(e)=>abrirModalCriarConta(e)}  href='#'>Criar Conta!</a>
     </div> 
 </div>
 }
